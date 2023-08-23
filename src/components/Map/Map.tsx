297cf8f124275c45d9b0bba50fe6840c47f08145
Stiliyan26 <stili262002@abv.bg>
2023-08-24 00:06:13 +0300

    map imported

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import styles from './Map.module.css';
+
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 export default function Map() {
+  const position: [number, number] = [51.505, -0.09];
+
   return (
-    <div>
-      
+    <div className={styles['map-container']}>
+      <MapContainer center={position} zoom={13} style={{ height: '100%', width: '100%' }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+      </MapContainer>
     </div>
   )
 }
